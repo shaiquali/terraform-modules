0c5f7171ee1da161ce22491fcbdbbfba722aea56
amiros89 <62113999+amiros89@users.noreply.github.com>
2021-10-26 14:01:30 +0300

    test

--- terraform/aws-elasticcache/main.tf
+++ terraform/aws-elasticcache/main.tf
@@ -1,10 +1,9 @@
-resource "aws_elasticache_replication_group" "example" {
-  automatic_failover_enabled    = true
-  availability_zones            = ["us-west-2a", "us-west-2b"]
-  replication_group_id          = "tf-rep-group-1"
-  replication_group_description = "test description"
-  node_type                     = "cache.m4.large"
-  number_cache_clusters         = 2
-  parameter_group_name          = "default.redis3.2"
-  port                          = 6379
+resource "aws_elasticache_cluster" "example" {
+  cluster_id           = "cluster-example"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
 }
