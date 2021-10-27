cd7d4c0bf2f8779a2ae637f060592277009c2b51
amiros89 <62113999+amiros89@users.noreply.github.com>
2021-10-27 12:02:07 +0300

    test

--- terraform/aws-elasticcache/network.tf
+++ terraform/aws-elasticcache/network.tf
@@ -8,11 +8,8 @@ resource "aws_vpc" "main" {
   enable_dns_support = true
 }
 
-resource "aws_subnet" "a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-west-1a"
-  map_public_ip_on_launch = true
+data "aws_subnet" "a" {
+  vpc_id     = "${var.vpc_id}"
 }
 
 resource "aws_subnet" "b" {
