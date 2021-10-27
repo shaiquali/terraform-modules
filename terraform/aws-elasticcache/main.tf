ce43db3d36d8e666153fb8799c96e076c8bfc95b
amiros89 <62113999+amiros89@users.noreply.github.com>
2021-10-27 10:47:19 +0300

    test

--- terraform/aws-elasticcache/main.tf
+++ terraform/aws-elasticcache/main.tf
@@ -4,6 +4,8 @@ resource "aws_elasticache_cluster" "example" {
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis3.2"
+  subnet_group_name = "elasticache-subnet"
+  security_group_ids = "elasticache-sg"
   engine_version       = "3.2.10"
   port                 = 6379
 }
