8b1dc5c41485f15d3eaeef7ed505e25f445c6630
amiros89 <62113999+amiros89@users.noreply.github.com>
2021-10-27 16:02:04 +0300

    test

--- terraform/aws-elasticcache/network.tf
+++ terraform/aws-elasticcache/network.tf
@@ -2,49 +2,18 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
+data "aws_vpc" "default" {
+  id = "vpc-0e1b62433c0c81943"
 }
 
-resource "aws_subnet" "a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-west-1a"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "eu-west-1b"
-    map_public_ip_on_launch = true
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-resource "aws_route_table" "all" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.gw.id
-    }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.a.id
-  route_table_id = aws_route_table.all.id
-}
-
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.b.id
-  route_table_id = aws_route_table.all.id
+data "aws_subnet_ids" "apps_subnets" {
+  vpc_id = "vpc-0e1b62433c0c81943"
+  tags = {
+    Name = "app-subnet*"
+  }
 }
 
 resource "aws_elasticache_subnet_group" "elasticache-subnet" {
   name       = "elasticache-subnet"
-  subnet_ids = [aws_subnet.a.id]
+  subnet_ids = [aws_subnet_ids.apps_subnets.id]
 }
