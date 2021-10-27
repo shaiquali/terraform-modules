17f008d8968458d2022e59e5638c47335b9a540e
amiros89 <62113999+amiros89@users.noreply.github.com>
2021-10-27 11:52:00 +0300

    test

--- terraform/aws-elasticcache/outputs.tf
+++ terraform/aws-elasticcache/outputs.tf
@@ -1,3 +1,7 @@
 output "hostname" {
   value = aws_elasticache_cluster.example.cache_nodes[0].address
 }
+
+output "security_group" {
+  value = data.aws_security_group.elasticache-sg.id
+}
--- terraform/aws-elasticcache/sg.tf
+++ terraform/aws-elasticcache/sg.tf
@@ -1,12 +1,13 @@
-resource "aws_security_group" "elasticache-sg" {
+data "aws_security_group" "elasticache-sg" {
   name        = "elasticache-sg"
   description = "Allow all inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = "${var.vpc_id}"
+  
 
-  ingress {
+ ingress {
     from_port   = 6379
     to_port     = 6379
     protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  } 
 }
