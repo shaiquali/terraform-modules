d5a29807d0f063bf92262b51161718b587dc6056
amiros89 <62113999+amiros89@users.noreply.github.com>
2021-11-22 11:56:17 +0200

    TEST

---
+++ terraform/sleep-short-term/outputs.tf
@@ -0,0 +1,30 @@
+output "hostname" {
+  value = "amir"
+}
+output "output1" {
+  value = "short term test"
+}
+output "output2" {
+  value = "short term test"
+}
+output "output3" {
+  value = "short term test"
+}
+output "output4" {
+  value = "short term test"
+}
+output "output5" {
+  value = "short term test"
+}
+output "output6" {
+  value = "short term test"
+}
+output "output7" {
+  value = "short term test"
+}
+output "output8" {
+  value = "short term test"
+}
+output "output9" {
+  value = "short term test"
+}
