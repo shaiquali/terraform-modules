e9b66e25ddbcb331afefeabe23fa359a75d0816c
amiros89 <62113999+amiros89@users.noreply.github.com>
2021-11-28 08:44:38 +0200

    set sg vpc id from variable

--- terraform/elasticache-postgres-modules/sg.tf
+++ terraform/elasticache-postgres-modules/sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "elasticache-sg" {
   name        = "elasticache-sg"
   description = "Allow all inbound traffic"
-  vpc_id      = "vpc-0e1b62433c0c81943"
+  vpc_id      = var.eks_vpc 
   
 
   ingress {
