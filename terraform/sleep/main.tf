ef6499b87905f146d79843c54e94fb9080c8598e
amiros89 <62113999+amiros89@users.noreply.github.com>
2021-11-28 15:58:27 +0200

    test

--- terraform/sleep/main.tf
+++ terraform/sleep/main.tf
@@ -4,3 +4,21 @@ resource "null_resource" "sleep" {
         interpreter = ["/bin/bash", "-c"]
   }
 }
+
+variable "my_name" {
+    default = "John Doe"
+}
+
+output message {
+  value       = "Hello World! my name is ${var.my_name}"
+  sensitive   = false
+  description = "My Message"
+  depends_on  = []
+}
+
+output url {
+  value       = "http://www.google.com"
+  sensitive   = false
+  description = "App URL"
+  depends_on  = []
+}
